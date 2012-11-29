9a83422d70043039b00f95ffb9f4ed084af4c034
Eric Bodden <eric.bodden@ec-spride.de>
2012-11-29 14:55:32 +0100

    license headers

--- src/heros/solver/SummaryFunctions.java
+++ src/heros/solver/SummaryFunctions.java
@@ -1,3 +1,13 @@
+/*******************************************************************************
+ * Copyright (c) 2012 Eric Bodden.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the GNU Lesser Public License v2.1
+ * which accompanies this distribution, and is available at
+ * http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+ * 
+ * Contributors:
+ *     Eric Bodden - initial API and implementation
+ ******************************************************************************/
 package heros.solver;
 
 import heros.EdgeFunction;
