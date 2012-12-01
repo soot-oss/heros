a72df676e2ffef1584c587a26ba51b2a800404e6
Eric Bodden <eric@bodden.de>
2012-12-01 23:10:24 +0100

    goto startline

--- heros-debugui/src/heros/debugui/EdgeDrawing.java
+++ heros-debugui/src/heros/debugui/EdgeDrawing.java
@@ -9,11 +9,15 @@ import org.eclipse.core.runtime.IPath;
 import org.eclipse.jdt.core.IJavaProject;
 import org.eclipse.jdt.core.JavaCore;
 import org.eclipse.jdt.core.JavaModelException;
+import org.eclipse.jface.text.BadLocationException;
+import org.eclipse.jface.text.IDocument;
+import org.eclipse.jface.text.IRegion;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.ui.IWorkbenchPage;
 import org.eclipse.ui.PartInitException;
 import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.ide.IDE;
+import org.eclipse.ui.texteditor.ITextEditor;
 
 public class EdgeDrawing {
 
@@ -37,14 +41,43 @@ public class EdgeDrawing {
 					IWorkbenchPage page = PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage();
 					IPath path = javaProject.findType(className).getPath();
 					IFile file = (IFile) ResourcesPlugin.getWorkspace().getRoot().findMember(path);
-					if(file!=null)
-						IDE.openEditor(page, file);
+					if(file!=null) {
+						ITextEditor editor = (ITextEditor) IDE.openEditor(page, file);
+						moveToLine(edge.startLine, editor);
+					}
+					
 				} catch (JavaModelException e) {
 					e.printStackTrace();
 				} catch (PartInitException e) {
 					e.printStackTrace();
 				}
 				
+			}
+
+			private void moveToLine(final int line, ITextEditor editor) {
+				IDocument document = editor.getDocumentProvider().getDocument(editor.getEditorInput());
+				System.err.println("!1");
+				if (document != null) {
+					System.err.println("!2");
+					IRegion lineInfo = null;
+					try {
+						// line count internaly starts with 0, and not
+						// with 1 like in
+						// GUI
+						lineInfo = document
+								.getLineInformation(line-1);
+					} catch (BadLocationException e) {
+						// ignored because line number may not really
+						// exist in document,
+						// we guess this...
+					}
+					System.err.println("!3 "+line);
+					if (lineInfo != null) {
+						System.err.println("!4");
+						editor.selectAndReveal(lineInfo.getOffset(),
+								lineInfo.getLength());
+					}
+				}
 			}
 		});
 	}
--- heros/src/heros/debugsupport/SerializableEdgeData.java
+++ heros/src/heros/debugsupport/SerializableEdgeData.java
@@ -4,12 +4,22 @@ import java.io.Serializable;
 
 public class SerializableEdgeData implements Serializable {
 
-	private static final long serialVersionUID = 833281837577489562L;
-
+	public static final long serialVersionUID = -6749133467770764139L;
+	
 	public final String className;
+	public final int startLine;
+	public final int startColumn;
+	public final int endLine;
+	public final int endColumn;
+	public final String label;
 
-	public SerializableEdgeData(String className) {
+	public SerializableEdgeData(String className, int startLine, int startColumn, int endLine, int endColumn, String label) {
 		this.className = className;
+		this.startLine = startLine;
+		this.startColumn = startColumn;
+		this.endLine = endLine;
+		this.endColumn = endColumn;
+		this.label = label;
 	} 
 	
 }
