ec6e9edc9974664938d3783f7f4f7aea82c2319a
Eric Bodden <eric@bodden.de>
2012-12-09 13:45:40 +0100

    toolbar button

---
+++ heros-debugui/src/heros/debugui/action/AnalyzeToolbarAction.java
@@ -0,0 +1,11 @@
+package heros.debugui.action;
+
+import org.eclipse.debug.ui.actions.AbstractLaunchToolbarAction;
+
+public class AnalyzeToolbarAction extends AbstractLaunchToolbarAction {
+
+	public AnalyzeToolbarAction() {
+		super("heros.launchGroups.analyze");
+	}
+
+}
