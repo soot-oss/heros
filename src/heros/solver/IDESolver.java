734c0ca4c2872518fde9728d9571daf9a652adbf
Eric Bodden <eric@bodden.de>
2013-07-06 18:50:04 +0200

    removed unused import

--- src/heros/solver/IDESolver.java
+++ src/heros/solver/IDESolver.java
@@ -30,7 +30,6 @@ import heros.edgefunc.EdgeIdentity;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
-import java.util.LinkedHashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
