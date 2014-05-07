b8ca40b04571cc38e767386133ba4757a98d6be1
Johannes Lerch <lerch@st.informatik.tu-darmstadt.de>
2014-05-07 12:53:48 +0200

    Tests for IDESolver

---
+++ test/heros/IFDSSolverTest.java
@@ -0,0 +1,96 @@
+/*******************************************************************************
+ * Copyright (c) 2014 Johannes Lerch.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the GNU Lesser Public License v2.1
+ * which accompanies this distribution, and is available at
+ * http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+ * 
+ * Contributors:
+ *     Johannes Lerch - initial API and implementation
+ ******************************************************************************/
+package heros;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static heros.TestHelper.*;
+
+public class IFDSSolverTest {
+
+	private TestHelper helper;
+
+	@Before
+	public void before() {
+		helper = new TestHelper();
+	}
+	
+	@Test
+	public void test() {
+		helper.method("bar", 
+				startPoints("a"),
+				normalStmt("a").succ("b", flow("0", "x")),
+				normalStmt("b").succ("c", flow("x", "x")),
+				callSite("c").calls("foo", flow("x", "y")).retSite("f", flow("x", "x")));
+		
+		helper.method("foo",
+				startPoints("d"),
+				normalStmt("d").succ("e", flow("y", "y", "z")),
+				exitStmt("e").returns(over("c"), to("f"), flow("z", "u"), flow("y")));
+		
+		helper.runSolver(false, "a");
+	}
+	
+	@Test
+	public void reuseSummary() {
+		helper.method("foo", 
+				startPoints("a"),
+				callSite("a").calls("bar", flow("0", "x")).retSite("b", flow("0", "y")),
+				callSite("b").calls("bar", flow("y", "x")).retSite("c", flow("y")),
+				normalStmt("c").succ("c0", flow("w", "0")));
+		
+		helper.method("bar",
+				startPoints("d"),
+				normalStmt("d").succ("e", flow("x", "z")),
+				exitStmt("e").returns(over("a"), to("b"), flow("z", "y"))
+							  .returns(over("b"), to("c"), flow("z", "w")));
+		
+		helper.runSolver(false, "a");
+	}
+	
+	@Test
+	public void branch() {
+		helper.method("foo",
+				startPoints("a"),
+				normalStmt("a").succ("b1", flow("0", "x")).succ("b2", flow("0", "x")),
+				normalStmt("b1").succ("c", flow("x", "x", "y")),
+				normalStmt("b2").succ("c", flow("x", "x")),
+				normalStmt("c").succ("d", flow("x", "z"), flow("y", "w")),
+				normalStmt("d").succ("e", flow("z"), flow("w")));
+		
+		helper.runSolver(false, "a");
+	}
+	
+	@Test
+	public void unbalancedReturn() {
+		helper.method("foo",
+				startPoints("a"),
+				normalStmt("a").succ("b", flow("0", "1")),
+				exitStmt("b").returns(over("x"),  to("y"), flow("1", "1")));
+		
+		helper.method("bar", 
+				startPoints("unused"),
+				normalStmt("y").succ("z", flow("1", "2")));
+		
+		helper.runSolver(true, "a");
+	}
+	
+	@Test
+	public void artificalReturnEdgeForNoCallersCase() {
+		helper.method("foo",
+				startPoints("a"),
+				normalStmt("a").succ("b", flow("0", "1")),
+				exitStmt("b").returns(null, null, flow("1", "1")));
+		
+		helper.runSolver(true, "a");
+	}
+}
---
+++ test/heros/TestHelper.java
@@ -0,0 +1,519 @@
+/*******************************************************************************
+ * Copyright (c) 2014 Johannes Lerch.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the GNU Lesser Public License v2.1
+ * which accompanies this distribution, and is available at
+ * http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
+ * 
+ * Contributors:
+ *     Johannes Lerch - initial API and implementation
+ ******************************************************************************/
+package heros;
+
+import static org.junit.Assert.assertTrue;
+import heros.solver.IFDSSolver;
+import heros.solver.Pair;
+
+import java.util.Collection;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import com.google.common.base.Function;
+import com.google.common.base.Joiner;
+import com.google.common.base.Predicate;
+import com.google.common.collect.HashMultimap;
+import com.google.common.collect.Iterables;
+import com.google.common.collect.Lists;
+import com.google.common.collect.Maps;
+import com.google.common.collect.Multimap;
+import com.google.common.collect.Sets;
+
+public class TestHelper {
+
+	private Multimap<Method, Statement> method2startPoint = HashMultimap.create();
+	private List<NormalEdge> normalEdges = Lists.newLinkedList();
+	private List<CallEdge> callEdges = Lists.newLinkedList();
+	private List<Call2ReturnEdge> call2retEdges = Lists.newLinkedList();
+	private List<ReturnEdge> returnEdges = Lists.newLinkedList();
+	private Map<Statement, Method> stmt2method = Maps.newHashMap();
+	private Set<ExpectedFlowFunction> remainingFlowFunctions = Sets.newHashSet();
+
+	public MethodHelper method(String methodName, Statement[] startingPoints, EdgeBuilder... edgeBuilders) {
+		MethodHelper methodHelper = new MethodHelper(new Method(methodName));
+		methodHelper.startPoints(startingPoints);
+		for(EdgeBuilder edgeBuilder : edgeBuilders){
+			methodHelper.edges(edgeBuilder.edges());
+		}
+		return methodHelper;
+	}
+
+	public static Statement[] startPoints(String... startingPoints) {
+		Statement[] result = new Statement[startingPoints.length];
+		for (int i = 0; i < result.length; i++) {
+			result[i] = new Statement(startingPoints[i]);
+		}
+		return result;
+	}
+
+	public static EdgeBuilder.NormalStmtBuilder normalStmt(String stmt) {
+		return new EdgeBuilder.NormalStmtBuilder(new Statement(stmt));
+	}
+	
+	public static EdgeBuilder.CallSiteBuilder callSite(String callSite) {
+		return new EdgeBuilder.CallSiteBuilder(new Statement(callSite));
+	}
+	
+	public static EdgeBuilder.ExitStmtBuilder exitStmt(String exitStmt) {
+		return new EdgeBuilder.ExitStmtBuilder(new Statement(exitStmt));
+	}
+	
+	public static Statement over(String callSite) {
+		return new Statement(callSite);
+	}
+	
+	public static Statement to(String returnSite) {
+		return new Statement(returnSite);
+	}
+	
+	public static ExpectedFlowFunction flow(String source, String... targets) {
+		Fact[] targetFacts = new Fact[targets.length];
+		for(int i=0; i<targets.length; i++) {
+			targetFacts[i] = new Fact(targets[i]);
+		}
+		return new ExpectedFlowFunction(new Fact(source), targetFacts);
+	}
+
+	public InterproceduralCFG<Statement, Method> buildIcfg() {
+		return new InterproceduralCFG<Statement, Method>() {
+
+			@Override
+			public boolean isStartPoint(Statement stmt) {
+				return method2startPoint.values().contains(stmt);
+			}
+
+			@Override
+			public boolean isFallThroughSuccessor(Statement stmt, Statement succ) {
+				throw new IllegalStateException();
+			}
+
+			@Override
+			public boolean isExitStmt(Statement stmt) {
+				for(ReturnEdge edge : returnEdges) {
+					if(edge.exitStmt.equals(stmt))
+						return true;
+				}
+				return false;
+			}
+
+			@Override
+			public boolean isCallStmt(final Statement stmt) {
+				return Iterables.any(callEdges, new Predicate<CallEdge>() {
+					@Override
+					public boolean apply(CallEdge edge) {
+						return edge.callSite.equals(stmt);
+					}
+				});
+			}
+
+			@Override
+			public boolean isBranchTarget(Statement stmt, Statement succ) {
+				throw new IllegalStateException();
+			}
+
+			@Override
+			public List<Statement> getSuccsOf(Statement n) {
+				LinkedList<Statement> result = Lists.newLinkedList();
+				for (NormalEdge edge : normalEdges) {
+					if (edge.includeInCfg && edge.unit.equals(n))
+						result.add(edge.succUnit);
+				}
+				return result;
+			}
+
+			@Override
+			public Collection<Statement> getStartPointsOf(Method m) {
+				return method2startPoint.get(m);
+			}
+
+			@Override
+			public Collection<Statement> getReturnSitesOfCallAt(Statement n) {
+				Set<Statement> result = Sets.newHashSet();
+				for (Call2ReturnEdge edge : call2retEdges) {
+					if (edge.includeInCfg && edge.callSite.equals(n))
+						result.add(edge.returnSite);
+				}
+				for(ReturnEdge edge : returnEdges) {
+					if(edge.includeInCfg && edge.callSite.equals(n))
+						result.add(edge.returnSite);
+				}
+				return result;
+			}
+
+			@Override
+			public Method getMethodOf(Statement n) {
+				return stmt2method.get(n);
+			}
+
+			@Override
+			public Set<Statement> getCallsFromWithin(Method m) {
+				throw new IllegalStateException();
+			}
+
+			@Override
+			public Collection<Statement> getCallersOf(Method m) {
+				Set<Statement> result = Sets.newHashSet();
+				for (CallEdge edge : callEdges) {
+					if (edge.includeInCfg && edge.destinationMethod.equals(m)) {
+						result.add(edge.callSite);
+					}
+				}
+				for (ReturnEdge edge : returnEdges) {
+					if (edge.includeInCfg && edge.calleeMethod.equals(m)) {
+						result.add(edge.callSite);
+					}
+				}
+				return result;
+			}
+
+			@Override
+			public Collection<Method> getCalleesOfCallAt(Statement n) {
+				List<Method> result = Lists.newLinkedList();
+				for (CallEdge edge : callEdges) {
+					if (edge.includeInCfg && edge.callSite.equals(n)) {
+						result.add(edge.destinationMethod);
+					}
+				}
+				return result;
+			}
+
+			@Override
+			public Set<Statement> allNonCallStartNodes() {
+				throw new IllegalStateException();
+			}
+		};
+	}
+
+	public void assertAllFlowFunctionsUsed() {
+		assertTrue("These Flow Functions were expected, but never used: \n" + Joiner.on(",\n").join(remainingFlowFunctions),
+				remainingFlowFunctions.isEmpty());
+	}
+
+	private void addOrVerifyStmt2Method(Statement stmt, Method m) {
+		if (stmt2method.containsKey(stmt) && !stmt2method.get(stmt).equals(m)) {
+			throw new IllegalArgumentException("Statement " + stmt + " is used in multiple methods: " + m + " and " + stmt2method.get(stmt));
+		}
+		stmt2method.put(stmt, m);
+	}
+
+	public MethodHelper method(Method method) {
+		MethodHelper h = new MethodHelper(method);
+		return h;
+	}
+
+	public class MethodHelper {
+
+		private Method method;
+
+		public MethodHelper(Method method) {
+			this.method = method;
+		}
+
+		public void edges(Collection<Edge> edges) {
+			for(Edge edge : edges) {
+				remainingFlowFunctions.addAll(Lists.newArrayList(edge.flowFunctions));
+				
+				edge.accept(new EdgeVisitor() {
+					@Override
+					public void visit(heros.TestHelper.ReturnEdge edge) {
+						addOrVerifyStmt2Method(edge.exitStmt, method);
+						edge.calleeMethod = method;
+						returnEdges.add(edge);
+					}
+					
+					@Override
+					public void visit(heros.TestHelper.Call2ReturnEdge edge) {
+						addOrVerifyStmt2Method(edge.callSite, method);
+						addOrVerifyStmt2Method(edge.returnSite, method);
+						call2retEdges.add(edge);
+					}
+					
+					@Override
+					public void visit(heros.TestHelper.CallEdge edge) {
+						addOrVerifyStmt2Method(edge.callSite, method);
+						callEdges.add(edge);
+					}
+					
+					@Override
+					public void visit(heros.TestHelper.NormalEdge edge) {
+						addOrVerifyStmt2Method(edge.unit, method);
+						addOrVerifyStmt2Method(edge.succUnit, method);
+						normalEdges.add(edge);
+					}
+				});
+			}
+		}
+
+		public void startPoints(Statement[] startingPoints) {
+			method2startPoint.putAll(method, Lists.newArrayList(startingPoints));
+		}
+	}
+	
+	public static class ExpectedFlowFunction {
+
+		public final Fact source;
+		public final Fact[] targets;
+		public Edge edge;
+
+		public ExpectedFlowFunction(Fact source, Fact... targets) {
+			this.source = source;
+			this.targets = targets;
+		}
+
+		@Override
+		public String toString() {
+			return String.format("%s: %s -> {%s}", edge, source, Joiner.on(",").join(targets));
+		}
+	}
+	
+	private static interface EdgeVisitor {
+		void visit(NormalEdge edge);
+		void visit(CallEdge edge);
+		void visit(Call2ReturnEdge edge);
+		void visit(ReturnEdge edge);
+	}
+
+	public static abstract class Edge {
+		public final ExpectedFlowFunction[] flowFunctions;
+		public boolean includeInCfg = true;
+
+		public Edge(ExpectedFlowFunction...flowFunctions) {
+			this.flowFunctions = flowFunctions;
+			for(ExpectedFlowFunction ff : flowFunctions) {
+				ff.edge = this;
+			}
+		}
+		
+		public abstract void accept(EdgeVisitor visitor);
+	}
+
+	public static class NormalEdge extends Edge {
+
+		private Statement unit;
+		private Statement succUnit;
+
+		public NormalEdge(Statement unit, Statement succUnit, ExpectedFlowFunction...flowFunctions) {
+			super(flowFunctions);
+			this.unit = unit;
+			this.succUnit = succUnit;
+		}
+
+		@Override
+		public String toString() {
+			return String.format("%s -normal-> %s", unit, succUnit);
+		}
+
+		@Override
+		public void accept(EdgeVisitor visitor) {
+			visitor.visit(this);
+		}
+	}
+
+	public static class CallEdge extends Edge {
+
+		private Statement callSite;
+		private Method destinationMethod;
+
+		public CallEdge(Statement callSite, Method destinationMethod, ExpectedFlowFunction...flowFunctions) {
+			super(flowFunctions);
+			this.callSite = callSite;
+			this.destinationMethod = destinationMethod;
+		}
+
+		@Override
+		public String toString() {
+			return String.format("%s -call-> %s", callSite, destinationMethod);
+		}
+		
+		@Override
+		public void accept(EdgeVisitor visitor) {
+			visitor.visit(this);
+		}
+	}
+
+	public static class Call2ReturnEdge extends Edge {
+		private Statement callSite;
+		private Statement returnSite;
+
+		public Call2ReturnEdge(Statement callSite, Statement returnSite, ExpectedFlowFunction...flowFunctions) {
+			super(flowFunctions);
+			this.callSite = callSite;
+			this.returnSite = returnSite;
+		}
+
+		@Override
+		public String toString() {
+			return String.format("%s -call2ret-> %s", callSite, returnSite);
+		}
+		
+		@Override
+		public void accept(EdgeVisitor visitor) {
+			visitor.visit(this);
+		}
+	}
+
+	public static class ReturnEdge extends Edge {
+
+		private Statement exitStmt;
+		private Statement returnSite;
+		private Statement callSite;
+		private Method calleeMethod;
+
+		public ReturnEdge(Statement callSite, Statement exitStmt, Statement returnSite, ExpectedFlowFunction...flowFunctions) {
+			super(flowFunctions);
+			this.callSite = callSite;
+			this.exitStmt = exitStmt;
+			this.returnSite = returnSite;
+			if(callSite == null || returnSite == null)
+				includeInCfg = false;
+		}
+
+		@Override
+		public String toString() {
+			return String.format("%s -return-> %s", exitStmt, returnSite);
+		}
+		
+		@Override
+		public void accept(EdgeVisitor visitor) {
+			visitor.visit(this);
+		}
+	}
+	
+	private static boolean nullAwareEquals(Object a, Object b) {
+		if(a == null)
+			return b==null;
+		else
+			return a.equals(b);
+	}
+
+	public FlowFunctions<Statement, Fact, Method> flowFunctions() {
+		return new FlowFunctions<Statement, Fact, Method>() {
+
+			@Override
+			public FlowFunction<Fact> getReturnFlowFunction(Statement callSite, Method calleeMethod, Statement exitStmt, Statement returnSite) {
+				for (final ReturnEdge edge : returnEdges) {
+					if (nullAwareEquals(callSite, edge.callSite) && edge.calleeMethod.equals(calleeMethod)
+							&& edge.exitStmt.equals(exitStmt) && nullAwareEquals(edge.returnSite, returnSite)) {
+						return createFlowFunction(edge);
+					}
+				}
+				throw new AssertionError(String.format("No Flow Function expected for return edge %s -> %s (call edge: %s -> %s)", exitStmt,
+						returnSite, callSite, calleeMethod));
+			}
+
+			@Override
+			public FlowFunction<Fact> getNormalFlowFunction(final Statement curr, final Statement succ) {
+				for (final NormalEdge edge : normalEdges) {
+					if (edge.unit.equals(curr) && edge.succUnit.equals(succ)) {
+						return createFlowFunction(edge);
+					}
+				}
+				throw new AssertionError(String.format("No Flow Function expected for %s -> %s", curr, succ));
+			}
+
+			@Override
+			public FlowFunction<Fact> getCallToReturnFlowFunction(Statement callSite, Statement returnSite) {
+				for (final Call2ReturnEdge edge : call2retEdges) {
+					if (edge.callSite.equals(callSite) && edge.returnSite.equals(returnSite)) {
+						return createFlowFunction(edge);
+					}
+				}
+				throw new AssertionError(String.format("No Flow Function expected for call to return edge %s -> %s", callSite, returnSite));
+			}
+
+			@Override
+			public FlowFunction<Fact> getCallFlowFunction(Statement callStmt, Method destinationMethod) {
+				for (final CallEdge edge : callEdges) {
+					if (edge.callSite.equals(callStmt) && edge.destinationMethod.equals(destinationMethod)) {
+						return createFlowFunction(edge);
+					}
+				}
+				throw new AssertionError(String.format("No Flow Function expected for call %s -> %s", callStmt, destinationMethod));
+			}
+
+			private FlowFunction<Fact> createFlowFunction(final Edge edge) {
+				return new FlowFunction<Fact>() {
+					@Override
+					public Set<Fact> computeTargets(Fact source) {
+						for (ExpectedFlowFunction ff : edge.flowFunctions) {
+							if (ff.source.equals(source)) {
+								if (remainingFlowFunctions.remove(ff)) {
+									return Sets.newHashSet(ff.targets);
+								} else {
+									throw new AssertionError(String.format("Flow Function '%s' was used multiple times on edge '%s'", ff, edge));
+								}
+							}
+						}
+						throw new AssertionError(String.format("Fact '%s' was not expected at edge '%s'", source, edge));
+					}
+				};
+			}
+		};
+	}
+
+	public void runSolver(final boolean followReturnsPastSeeds, final String...initialSeeds) {
+		final InterproceduralCFG<Statement, Method> icfg = buildIcfg();
+		final FlowFunctions<Statement, Fact, Method> flowFunctions = flowFunctions();
+		IFDSSolver<Statement, Fact, Method, InterproceduralCFG<Statement, Method>> solver = new IFDSSolver<>(
+				new IFDSTabulationProblem<Statement, Fact, Method, InterproceduralCFG<Statement, Method>>() {
+
+					@Override
+					public boolean followReturnsPastSeeds() {
+						return followReturnsPastSeeds;
+					}
+
+					@Override
+					public boolean autoAddZero() {
+						return false;
+					}
+
+					@Override
+					public int numThreads() {
+						return 1;
+					}
+
+					@Override
+					public boolean computeValues() {
+						return false;
+					}
+
+					@Override
+					public FlowFunctions<Statement, Fact, Method> flowFunctions() {
+						return flowFunctions;
+					}
+
+					@Override
+					public InterproceduralCFG<Statement, Method> interproceduralCFG() {
+						return icfg;
+					}
+
+					@Override
+					public Map<Statement, Set<Fact>> initialSeeds() {
+						Map<Statement, Set<Fact>> result = Maps.newHashMap();
+						for (String stmt : initialSeeds) {
+							result.put(new Statement(stmt), Sets.newHashSet(new Fact("0")));
+						}
+						return result;
+					}
+
+					@Override
+					public Fact zeroValue() {
+						return new Fact("0");
+					}
+				});
+
+		solver.solve();
+		assertAllFlowFunctionsUsed();
+	}
+}
