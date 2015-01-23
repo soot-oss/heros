6939a4729e754c10960e0f7de250274caa120223
Alexandre Bartel <alexandre.bartel@cased.de>
2015-01-23 13:34:11 +0100

    fix: 'bottom(id)' should return 'bottom' not 'id'. Thanks to Johannes Späth who found the bug.

--- src/heros/edgefunc/AllBottom.java
+++ src/heros/edgefunc/AllBottom.java
@@ -26,6 +26,8 @@ public class AllBottom<V> implements EdgeFunction<V> {
 	}
 
 	public EdgeFunction<V> composeWith(EdgeFunction<V> secondFunction) {
+		if (secondFunction instanceof EdgeIdentity)
+			return this;
 		return secondFunction;
 	}
 
