ee6cd14ce45f8e1a2ac7a25d6fe648f4496e9d89
Johannes Lerch <lerch.johannes@googlemail.com>
2015-04-16 15:27:30 +0200

    hashcode&equals in ZeroCallEdgeResolver to avoid infinite amounts of facts

--- src/heros/alias/ZeroCallEdgeResolver.java
+++ src/heros/alias/ZeroCallEdgeResolver.java
@@ -35,4 +35,30 @@ public class ZeroCallEdgeResolver<Field, Fact, Stmt, Method> extends CallEdgeRes
 	protected ZeroCallEdgeResolver<Field, Fact, Stmt, Method> getOrCreateNestedResolver(AccessPath<Field> newAccPath) {
 		return this;
 	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result
+				+ ((zeroHandler == null) ? 0 : zeroHandler.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		ZeroCallEdgeResolver other = (ZeroCallEdgeResolver) obj;
+		if (zeroHandler == null) {
+			if (other.zeroHandler != null)
+				return false;
+		} else if (!zeroHandler.equals(other.zeroHandler))
+			return false;
+		return true;
+	}
 }
